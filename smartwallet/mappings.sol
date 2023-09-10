116a2cc1089a74bdc37a186a13844b45c0ef337f
therevealer <officialnfluence@gmail.com>
2023-09-11 00:04:28 +0100

    Added mapping

--- smartwallet/mappings.sol
+++ smartwallet/mappings.sol
@@ -1,2 +1,15 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+contract SimpleMapping{
+    mapping(uint => bool) public myMapping;
+    mapping(address => bool) public myAddressMapping;
+
+    function setValue(uint _index) public{
+        myMapping[_index] = true;
+    }
+
+     function setMyAddressToTrue() public {
+        myAddressMapping[msg.sender] = true;
+    }
+}
