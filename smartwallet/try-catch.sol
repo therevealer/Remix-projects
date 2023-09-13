c8dabf5860a25f3514dc7571c23f9571838cbda3
therevealer <officialnfluence@gmail.com>
2023-09-13 23:49:37 +0100

    Replaced try and catch for require and if for Eth transfer Function

--- smartwallet/try-catch.sol
+++ smartwallet/try-catch.sol
@@ -1,38 +1,48 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract tryandCatch {
-    address payable  public owner;
-    uint public fundingGoal;
-    uint public totalFunds;
+contract FundWithdrawalExample {
+    address payable public owner;
+    uint256 public fundingGoal;
+    uint256 public totalFunds;
 
-    constructor(uint _goal){
-        owner = payable (msg.sender);
+    constructor(uint256 _goal) {
+        owner = payable(msg.sender);
         fundingGoal = _goal;
     }
 
-    modifier onlyOwner () {
+    modifier onlyOwner() {
         require(msg.sender == owner, "Only the owner can call this function");
         _;
     }
-    
 
+    // Function for contributors to send Ether
     function contribute() external payable {
         require(msg.value > 0, "Contribution must be greater than 0");
         totalFunds += msg.value;
     }
 
-    function withdrawFunds() external onlyOwner{
+    // Function for the owner to withdraw funds
+    function withdrawFunds() external onlyOwner {
+        // Check if the funding goal is met
         require(totalFunds >= fundingGoal, "Funding goal not met");
         
-      try  owner.transfer(totalFunds){
-          totalFunds = 0;
-
-      } catch(bytes memory errorData){
-
-          emit WithdrawalError(errorData);
-      }
+        // Attempt to transfer funds to the owner
+        bool success = owner.send(totalFunds);
+        if (success) {
+            totalFunds = 0; // Reset total funds after successful transfer
+        } else {
+            // Handle the withdrawal error gracefully
+            // Log the error or take appropriate action
+            emit WithdrawalError("Transfer failed");
+        }
     }
+
+    // Function to check the current balance of the contract
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+
     // Event to log withdrawal errors
-    event WithdrawalError(bytes errorData);
+    event WithdrawalError(string error);
 }
